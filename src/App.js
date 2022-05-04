ea0accef4b38d96557b27b72a54eea4305823948
Weslley Santos <weslleyalmerindo2@gmail.com>
2022-05-04 03:01:39 -0300

    features: clock logic working

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-let secondsIntervalId;
-let minutesIntervalId;
+let intervalId;
 
 const App = () => {
-  const [seconds, setSeconds] = useState('00');
-  const [minutes, setMinutes] = useState('00');
-  const [time, setTime] = useState('0');
+  const [secondsAmount, setSecondsAmount] = useState('0');
 
-  const startTimer = () => {
-    secondsIntervalId = setInterval(() => { setSeconds((prevState) => parseInt(prevState, 10) + 1) }, 1000);
-    console.log(Number(time));
-    const time2 = parseInt(time, 10) * 1000;
-    console.log(time2);
-    setTimeout(() => { clearInterval(secondsIntervalId) }, time2); // Late it have to be the time typed in Input
+  const minutes = Math.floor(Number(secondsAmount) / 60);
+  const seconds = Number(secondsAmount) % 60;
+
+  const handleChange = ({ target: { value } }) => {
+    setSecondsAmount(Number(value) * 60);
   }
 
-  const handleChange = ({ target }) => {
-    setTime(target.value);
+  const handleClick = () => {
+    if (Number(secondsAmount) > 0) {
+      intervalId = setInterval(() => {
+        setSecondsAmount(prevState => prevState - 1);
+      }, 1000);
+    }
   }
 
-  return (
+  useEffect(() => {
+    if (secondsAmount === 0) {
+      global.alert('Time is over!');
+      clearInterval(intervalId);
+    }
+  }, [secondsAmount]);
+
+  return(
     <main>
-      <h1 className="counter">{ `${minutes}:${seconds}` }</h1>
-      <div className="buttons-container">
+      <h1 className="time">
+        { `${String(minutes).padStart(2, 0)}:${String(seconds).padStart(2, 0)}` }
+      </h1>
+      <div className="time-components">
         <input
           type="number"
-          min="0"
-          placeholder="Minutos"
-          value={ time }
-          className="time-input"
+          min="1"
           onChange={ handleChange }
         />
-        <button type="button" className="start-button" onClick={ startTimer }>
-          Go!
+        <button
+          type="button"
+          onClick={ handleClick }
+        >
+          Start
         </button>
-        <button type="button" className="stop-button">
-          Stop!
+        <button
+          type="button"
+          onClick={ () => clearInterval(intervalId) }
+        >
+          Stop
         </button>
       </div>
     </main>
-  );
+  )
 }
 
 export default App;
