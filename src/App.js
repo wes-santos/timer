0c10684da9b02efb3b69a269dbeb57f575b8f3b9
Weslley Santos <weslleyalmerindo2@gmail.com>
2022-05-21 21:24:11 -0300

    feat: clock working without style and extra functions

--- src/App.js
+++ src/App.js
@@ -1,56 +1,112 @@
 import React, { useState, useEffect } from 'react';
+import tempo from './tempo.mp3';
 
 let intervalId;
+let timeoutId;
 
 const App = () => {
-  const [secondsAmount, setSecondsAmount] = useState('0');
-
-  const minutes = Math.floor(Number(secondsAmount) / 60);
-  const seconds = Number(secondsAmount) % 60;
-
-  const handleChange = ({ target: { value } }) => {
-    setSecondsAmount(Number(value) * 60);
-  }
-
-  const handleClick = () => {
-    if (Number(secondsAmount) > 0) {
-      intervalId = setInterval(() => {
-        setSecondsAmount(prevState => prevState - 1);
-      }, 1000);
-    }
-  }
+  const [originalSeconds, setOriginalSeconds] = useState(0);
+  const [originalMinutes, setOriginalMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [audio] = useState(new Audio(tempo));
+  const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
-    if (secondsAmount === 0) {
-      global.alert('Time is over!');
-      clearInterval(intervalId);
-    }
-  }, [secondsAmount]);
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
 
-  return(
+  useEffect(() => {
+    if (seconds === 0 && minutes > 0) {
+      setMinutes(prev => prev - 1);
+      setSeconds(59);
+    }
+  }, [seconds]);
+
+  const handleStart = () => {
+    setPlaying(false);
+    intervalId = setInterval(() => {
+      setSeconds(prev => prev - 1);
+    }, 1000);
+
+    timeoutId = setTimeout(() => {
+      clearInterval(intervalId);
+      setPlaying(true);
+    }, minutes * 60 * 1000 + seconds * 1000);
+  };
+
+  const handleMinutes = ({target: { value }}) => {
+    setOriginalMinutes(Number(value));
+    setMinutes(Number(value));
+  };
+
+  const handleSeconds = ({ target: { value } }) => {
+    if (seconds > 58) {
+      setMinutes(prev => prev + 1);
+      return setSeconds(0);
+    }
+    setOriginalSeconds(Number(value));
+    setSeconds(Number(value));
+  };
+
+  const handleStop = () => {
+    clearInterval(intervalId);
+    clearTimeout(timeoutId);
+  };
+
+  const handleRestart = () => {
+    setMinutes(originalMinutes);
+    setSeconds(originalSeconds);
+    clearTimeout(timeoutId);
+    clearInterval(intervalId);
+    handleStart();
+  };
+
+  return (
     <main>
-      <h1 className="time">
-        { `${String(minutes).padStart(2, 0)}:${String(seconds).padStart(2, 0)}` }
-      </h1>
-      <div className="time-components">
-        <input
-          type="number"
-          min="1"
-          onChange={ handleChange }
-        />
-        <button
-          type="button"
-          onClick={ handleClick }
-        >
-          Start
-        </button>
-        <button
-          type="button"
-          onClick={ () => clearInterval(intervalId) }
-        >
-          Stop
-        </button>
+      <div className="center">
+        <div className="time">
+          {`${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`}
+        </div>
       </div>
+      <div className="center">
+          <input
+            type="number"
+            min="0"
+            value={minutes}
+            onChange={ handleMinutes }
+          />
+          <input
+            type="number"
+            min="0"
+            value={seconds}
+            onChange={ handleSeconds }
+          />
+        </div>
+        <div className="center-row">
+          <button
+            type="button"
+            onClick={ handleStart }
+            className="button"
+          >
+            Start
+          </button>
+          <button
+            type="button"
+            onClick={ handleStop }
+            className="button"
+          >
+            Stop
+          </button>
+
+          <button
+            type="button"
+            onClick={ handleRestart }
+            className="button"
+          >
+            Restart
+          </button>
+        </div>
     </main>
   )
 }
